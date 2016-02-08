da040e2c8fc277fb40cd9e271e6b590b8b91debf
alexandre <alexandre.julien.91@gmail.com>
2016-02-08 16:12:51 +0100

    accueil

--- application/views/include/include_css.php
+++ application/views/include/include_css.php
@@ -1,2 +1,3 @@
 <link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="/assets/css/menu-haut.css">
+<link rel="stylesheet" type="text/css" href="/assets/css/font-awesome.min.css">
