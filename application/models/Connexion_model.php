9a8bba1286a0aef621cb5fd65bd3537ce7947c8b
Rémy <remy.papigny@gmail.com>
2016-02-08 17:06:10 +0100

    Script de connexion

---
+++ application/models/Connexion_model.php
@@ -0,0 +1,25 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Connexion_model extends CI_Model
+{   
+    //----------------------- Page connexion -----------------------
+    
+    public function verifierIdentifiant($identifiant, $mdp)
+    {
+        $tab['erreur'] = '';
+        
+        $this->db->select('id, login, password, mail');
+        $this->db->from('user');
+        $this->db->where('password', $mdp);
+        $this->db->where('(mail = "'.$identifiant.'" OR login = "'.$identifiant.'")');
+        $query = $this->db->get();
+        
+        $nb = $query->num_rows();
+        $result = $query->result();
+        
+        if($nb == 0)
+            $tab['erreur'] = "Identifiant ou mot de passe incorrect.";
+        
+        return $tab;
+    }
+}
