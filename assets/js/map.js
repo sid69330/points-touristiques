bb362fa10d5cd1400ee481b36aeeca225f1c8f5a
anthony <millota.am@gmail.com>
2016-02-08 14:48:05 +0100

    Ajout fichier JSON

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,28 +1,28 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
-	center: {lat: -34.397, lng: 150.644},
-	zoom: 6
+	center: {lat: 45.764043, lng: 4.835658999999964},
+	zoom: 12
   });
   var infoWindow = new google.maps.InfoWindow({map: map});
 
   // Try HTML5 geolocation.
- //  if (navigator.geolocation) {
-	// navigator.geolocation.getCurrentPosition(function(position) {
-	//   var pos = {
-	// 	lat: position.coords.latitude,
-	// 	lng: position.coords.longitude
-	//   };
+  if (navigator.geolocation) {
+	navigator.geolocation.getCurrentPosition(function(position) {
+	  var pos = {
+		lat: position.coords.latitude,
+		lng: position.coords.longitude
+	  };
 
-	//   infoWindow.setPosition(pos);
-	//   infoWindow.setContent('Je suis ici');
-	//   map.setCenter(pos);
-	// }, function() {
-	//   handleLocationError(true, infoWindow, map.getCenter());
-	// });
- //  } else {
-	// // Browser doesn't support Geolocation
-	// handleLocationError(false, infoWindow, map.getCenter());
- //  }
+	  infoWindow.setPosition(pos);
+	  infoWindow.setContent("<div id='hereyouare'>Je suis ici</div>");
+	  map.setCenter(pos);
+	}, function() {
+	  handleLocationError(true, infoWindow, map.getCenter());
+	});
+  } else {
+	// Browser doesn't support Geolocation
+	handleLocationError(false, infoWindow, map.getCenter());
+  }
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
