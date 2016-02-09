3b0feacea716f845c08d76172231a982f7da007e
alexandre <alexandre.julien.91@gmail.com>
2016-02-09 15:59:15 +0100

    Pseudo dans menu haut + modal confirm déconnexion

--- application/controllers/Deconnexion.php
+++ application/controllers/Deconnexion.php
@@ -1,7 +1,7 @@
 <?php
 
 class Deconnexion extends CI_Controller
-{	
+{
 	public function __construct()
 	{
 		parent::__construct();
--- application/views/include/menu-haut.php
+++ application/views/include/menu-haut.php
@@ -3,12 +3,21 @@
 	<div id="titreSite">
 		Touristix
 	</div>
+	<?php if($this->session->userdata('connexion')['id'] != false)
+	{
+	?>
+		<div id="pseudoConnecte" class="visible-lg visible-md visible-sm">
+			Connecté en tant que <?php echo $this->session->userdata('connexion')['pseudo']; ?>
+		</div>
+	<?php
+	}
+	?>
 </div>
 <div class="container-fluid">
 	<nav id="bt-menu" class="bt-menu">
 		<ul style="margin:0;padding:0">
 			<li ><a href="/"><i class="fa fa-home"></i> Accueil</a></li>
-			<?php if($this->session->userdata('connexion')['pseudo'] == false)
+			<?php if($this->session->userdata('connexion')['id'] == false)
 			{
 			?>
 				<li ><a href="/connexion"><i class="fa fa-sign-in"></i> Connexion</a></li>
@@ -16,7 +25,7 @@
 			<?php
 			}
 			else { ?>
-				<li ><a href="/deconnexion"><i class="fa fa-sign-out"></i> Déconnexion</a></li>
+				<li ><a href="#" data-toggle="modal" data-target="#modalDeconnexion"><i class="fa fa-sign-out"></i> Déconnexion</a></li>
 			<?php
 			}
 				if(isset($categ))
@@ -28,4 +37,23 @@
 				} 	?>
 		</ul>
 	</nav>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="modalDeconnexion" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  	<div class="modal-dialog" role="document">
+    	<div class="modal-content">
+      		<div class="modal-header">
+        		<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        		<h4 class="modal-title" id="myModalLabel">Déconnexion</h4>
+      		</div>
+      		<div class="modal-body">
+        		Êtes-vous sûr de vouloir vous déconnecter ?
+      		</div>
+     		 <div class="modal-footer">
+        		<button type="button" class="btn btn-default" data-dismiss="modal">Annuler</button>
+        		<a href="/deconnexion" class="btn btn-danger">Se déconnecter</a>
+      		</div>
+    	</div>
+  	</div>
 </div>
