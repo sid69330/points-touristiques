c0a769ac111606cc10be6c9bd81bf088d3415fd0
alexandre <alexandre.julien.91@gmail.com>
2016-02-10 15:23:43 +0100

    js en bas de page

--- application/views/index.php
+++ application/views/index.php
@@ -5,7 +5,6 @@
 	<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
 	<meta charset="utf-8">
 	<?php include_once($_SERVER['DOCUMENT_ROOT'].'/application/views/include/include_css.php'); ?>
-	<script type="text/javascript" src="/assets/js/modernizr.custom.js"></script>
 	<style>
 	  html, body {
 		height: 100%;
@@ -44,11 +43,11 @@
 	<?php
 	}
 	?>
-
 	<div id="map"></div>
 	<script src="/assets/js/map.js"></script>
 	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyABmaswDuz7-HVvHoHgrqYdmunESX84x9s&signed_in=true&callback=initMap" async defer></script>
 	<script type="text/javascript" src="/assets/js/jquery-1-11-2.js"></script>
+	<script type="text/javascript" src="/assets/js/modernizr.custom.js"></script>
 	<script type="text/javascript" src="/assets/js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="/assets/js/menu-haut.js"></script>
 	<script type="text/javascript" src="/assets/js/api.js"></script>
