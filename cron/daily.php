cf1424a8ea604e0c616c04d7bd0ec1d7bab48b40
SamuelPouzet <samuel.pouzet@yahoo.fr>
2016-02-11 12:25:56 +0100

    correction du bug de l'encodage json

--- cron/daily.php
+++ cron/daily.php
@@ -1,6 +1,7 @@
 <?php
-// cron journalié pour récupérer les datas sur l'api du grand lyon
+// cron journalier pour récupérer les datas sur l'api du grand lyon
 $path = dirname(__FILE__).'/../assets/json/data.json';
 $data = file_get_contents('https://download.data.grandlyon.com/wfs/rdata?SERVICE=WFS&VERSION=2.0.0&outputformat=GEOJSON&request=GetFeature&typename=sit_sitra.sittourisme');
+$data = mb_convert_encoding($data, 'HTML-ENTITIES');
 file_put_contents($path, $data);
 ?>
